8f4fcf7033d24657bed38763d0792e322feecd07
Lyrycs <aoref10.1@gmail.com>
2020-07-24 17:55:14 +0200

    Refactor XMLReader to not download file everytime

---
+++ Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Albion_RMT_Empire_Tool_Beta
+{
+    static class Constants
+    {
+        public static String URLStringItems = "http://blamedevs.com/items.xml";
+        public static String URLStringData = "http://blamedevs.com/data.xml";
+    }
+}
---
+++ CultureClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Albion_RMT_Empire_Tool_Beta
+{
+    static class CultureClass
+    {
+        public static CultureInfo GetCultureInfo(CultureInfo info)
+        {
+            info.NumberFormat.NumberDecimalSeparator = ",";
+            info.NumberFormat.NumberGroupSeparator = "";
+            return info;
+        }
+    }
+}
